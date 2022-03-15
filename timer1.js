84c6080906c9eda8206cf49bedcccd253f15d027
Jarrett Kirck <jkirck@hotmail.com>
2022-03-15 17:49:43 -0400

    simpleTimer

---
+++ timer1.js
@@ -0,0 +1,14 @@
+let args = process.argv;
+args = args.slice(2);
+
+function simpleTimer (num) {
+    let count = 0
+    for (let i = 0; i < num.length; i ++){
+        count = Number(num[i]);
+        setTimeout(() => {console.log(num[i])},count * 1000)
+        process.stdout.write('\x07');
+    }
+}
+//console.log(simpleTimer(args));
+
+simpleTimer(5);
